e45ef12c2cf623b20b637b329b5eff0e8449882f
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-03-23 03:33:47 +0500

    migration

---
+++ src/PersonalFinance.Infrastructure/Migrations/20240321112437_DbUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinance.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/PersonalFinance.Infrastructure/Migrations/20240321143843_Ok.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinance.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ok : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
